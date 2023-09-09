18675fa64e7becd5c2d21c8b44a6bc4040d0a13b
PirAdnan1 <109432989+PirAdnan1@users.noreply.github.com>
2023-09-09 06:33:46 -0700

    fix button

--- src/home/SectionC.jsx
+++ src/home/SectionC.jsx
@@ -1,47 +1,50 @@
-import React from 'react'
-
+import React from "react";
 
 // components
-import Button from '../Components/button'
-import Container from '../Mycontainer/Container'
+import Button from "../Components/button";
+import Container from "../Mycontainer/Container";
 
 // assets
-import ScreenVector from '../assets/ScreenVector'
-import WatchVector from '../assets/WatchVector'
-import EmojiVector from '../assets/EmojiVector'
+import ScreenVector from "../assets/ScreenVector";
+import WatchVector from "../assets/WatchVector";
+import EmojiVector from "../assets/EmojiVector";
 import bgCol from "../assets/bgCol.png";
 
-
 function SectionC() {
-
-
   return (
-    <Container >
+    <Container>
       <div className="">
-      <div className='grid lg:grid-cols-3  xl:mx-10 px-4 gap-7 sm:px-0 justify-center justify-items-center mt-20'>
-        <div className='shadow-md mt-3 w-full max-w-[415px] pt-12 justify-center items-center flex flex-col'>
-        <ScreenVector />
-        <span className='my-8 text-center'>Intuitive interface</span>
-        </div>
-        <div className='shadow-md mt-12 w-full max-w-[415px] lg:mt-0 pt-12 justify-center items-center flex flex-col'>
+        <div className="grid lg:grid-cols-3  xl:mx-10 px-4 gap-7 sm:px-0 justify-center justify-items-center mt-20">
+          <div className="shadow-md mt-3 w-full max-w-[415px] pt-12 justify-center items-center flex flex-col">
+            <ScreenVector />
+            <span className="my-8 text-center">Intuitive interface</span>
+          </div>
+          <div className="shadow-md mt-12 w-full max-w-[415px] lg:mt-0 pt-12 justify-center items-center flex flex-col">
             <WatchVector />
-            <span className='text-center w-[368px] my-8'>Massive time savings for entire staff</span>
-        </div>
-        <div className='shadow-md mt-12 w-full max-w-[415px] lg:mt-0 pt-12 justify-center items-center flex flex-col'>
+            <span className="text-center w-[368px] my-8">
+              Massive time savings for entire staff
+            </span>
+          </div>
+          <div className="shadow-md mt-12 w-full max-w-[415px] lg:mt-0 pt-12 justify-center items-center flex flex-col">
             <EmojiVector />
-            <span className='my-8'>Easy to use</span>
+            <span className="my-8">Easy to use</span>
+          </div>
         </div>
+        <div className="relative">
+          <img
+            src={bgCol}
+            alt="..."
+            width={346}
+            className="w-full relative"
+            height={296}
+          />
+          <div className="absolute bottom-12 translate-x-[32rem] z-10">
             <Button />
-         
-    </div>
-     <div className=''>
-  
-    <img src={bgCol}  alt='...' width={346} className='w-full relative' height={296} />
-   
-    </div> 
-    </div>
+          </div>
+        </div>
+      </div>
     </Container>
-  )
+  );
 }
 
-export default SectionC
+export default SectionC;
